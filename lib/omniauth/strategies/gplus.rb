9bcbab15e0b9f854be08412d1ccef10e94ff3a98
Paul Battley <pbattley@gmail.com>
2014-07-08 16:26:15 +0100

    Don't prefix OpenID Connect scopes
    > Scopes that conform to the OpenID Connect standard have full names
    > that are short: profile, email and openid—they are not in the form of
    > a URI. On the other hand, Google-specific scopes are in the form of a
    > URI, such as https://www.googleapis.com/auth/plus.login.
    
    See https://developers.google.com/+/api/oauth

--- lib/omniauth/strategies/gplus.rb
+++ lib/omniauth/strategies/gplus.rb
@@ -65,8 +65,14 @@ module OmniAuth
         scopes.split(/,\s*/).collect(&method(:format_scope)).join(' ')
       end
 
+      OPENID_CONNECT_SCOPES = %w( profile email openid )
+
       def format_scope(scope)
-        "https://www.googleapis.com/auth/#{scope}"
+        if OPENID_CONNECT_SCOPES.include?(scope)
+          scope
+        else
+          "https://www.googleapis.com/auth/#{scope}"
+        end
       end
 
       def custom_parameters(params)
